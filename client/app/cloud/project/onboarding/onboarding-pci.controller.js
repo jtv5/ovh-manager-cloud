5bccfd3ee6b5b954e3eaaab146fc64b17fa283f0
varun257 <varun257@gmail.com>
2019-02-28 10:43:02 +0530

    fix(cloud-pci): add tracking
    add tracking on new pci menu
    
    resolves MBE-283, MBE-284

--- client/app/cloud/project/onboarding/onboarding-pci.controller.js
+++ client/app/cloud/project/onboarding/onboarding-pci.controller.js
@@ -1,9 +1,10 @@
 class pciSlideshowCtrl {
-  constructor($scope, $state, $stateParams, $uibModalInstance, $window, ovhUserPref) {
+  constructor($scope, $state, $stateParams, $uibModalInstance, $window, atInternet, ovhUserPref) {
     this.$state = $state;
     this.$scope = $scope;
     this.$uibModalInstance = $uibModalInstance;
     this.$window = $window;
+    this.atInternet = atInternet;
     this.projectId = $stateParams.projectId;
     this.ovhUserPref = ovhUserPref;
   }
@@ -14,7 +15,13 @@ class pciSlideshowCtrl {
     });
   }
 
-  dismiss() {
+  dismiss(discover) {
+    if (discover) {
+      this.atInternet.trackClick({
+        name: 'notification_new_menu_pci_5',
+        type: 'action',
+      });
+    }
     this.$uibModalInstance.close();
     this.$state.go('iaas.pci-project.compute.infrastructure.diagram', {
       projectId: this.projectId,
